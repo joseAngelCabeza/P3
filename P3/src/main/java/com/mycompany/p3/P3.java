0615f22336f043a60dcc27c37f574d65679d9543
carlo <carlo@DAW50>
2024-01-10 10:40:03 +0100

    Commit: añadir p3java y pom.xml

---
+++ P3/src/main/java/com/mycompany/p3/P3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p3;
+
+/**
+ *
+ * @author carlo
+ */
+public class P3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Adios");
+    }
+}
